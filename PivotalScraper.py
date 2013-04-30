0b24231c9aaa97c34f7bfe86433e9cb187a891df
Ben Taylor <bewt85@gmail.com>
2013-04-30 01:32:29 +0100

    JSONSoup can findAll and soups have parents and children

--- PivotalScraper.py
+++ PivotalScraper.py
@@ -37,37 +37,107 @@ class PivotalScraper():
     return self.get('https://www.pivotaltracker.com/services/v5/projects/%s' % project_id)
 
 class JSONSoup:
+  """ Not really a proper Soup but certainly inspired by BeautifulSoup """
   def __init__(self, raw=None):
-    if raw=None:
-      self.raw = None
+    if raw==None:
+      raw = {}
     elif type(raw) == list or type(raw) == dict:
-      self.raw = raw
+      raw = raw
     elif type(raw) == str:
-      self.raw=simplejson.loads(json_text)
+      raw=simplejson.loads(json_text)
     else:
       # I could allow things like 'int's as well but then it would be ambiguous
       # whether a string is actually a string or a bit of JSON.
       raise TypeError("JSON soup cannot be initialised with this type")
-      
+    self.buildTree(raw)
+  
+  """
+  def __repr__(self):
+    return self.__str__()
+  """
+   
   def __str__(self):
     """ NB JSON null <--> Python None """
-    return simplejson.dumps(self.raw, indent=2)
-
-  def __getattr__(self, name):
-    return raw[name]
-    
+    def build(soup):
+      if type(soup.children) == list:
+        raw = []
+        for c in soup.children:
+          if isinstance(c, JSONSoup):
+            raw.append(build(c))
+          else:
+            raw.append(c)
+        return raw
+      elif type(soup.children) == dict:
+        raw = {}
+        for k,c in soup.children.iteritems():
+          if isinstance(c, JSONSoup):
+            raw.update({k: build(c)})
+          else:
+            raw.update({k: c})
+        return raw
+      
+    return build(self).__str__()
+  
   def __getitem__(self, k):
-    return raw[k]
+    return self.children[k]
     
   def __setitem__(self, k,v):
-    raw[k] = v
+    self.children[k] = self.v
+
+  def __len__(self):
+    return len(self.children)
+
+  def buildTree(self, raw):
+    self.parent = None
+    if type(raw) == list:
+      self.children = []
+      for c in raw:
+        if type(c) in [list, dict]:
+          child = JSONSoup(c)
+          child.parent=self
+        else:
+          child = c
+        self.children.append(child)
+    elif type(raw) == dict:
+      self.children = {}
+      for k,c in raw.iteritems():
+        if type(c) in [list, dict]:
+          child = JSONSoup(c)
+          child.parent = self
+        else:
+          child = c
+        self.children.update({k: child})
+    else:
+      raise TypeError("Should only have dicts and lists at this stage")
 
   def findAll(self, attrs):
-    def hunt(d):
+    def hunt():
+      for soup in self.findKey(attrs.keys()):
+        match=True
+        for (k,v) in attrs.iteritems():
+          if type(soup.children) <> dict: #this shouldn't ever be the case
+            match=False
+            break
+          if k not in soup.children.keys() or soup.children[k] <> v:
+            match=False
+            break
+        if match:
+          yield soup
+          
+    results = [result for result in hunt()]
+    result_soup = JSONSoup(results)
+    return result_soup
+
+  def findKey(self, keys):
+    key_set = set(keys)
+    def hunt(soup):
+      if isinstance(soup, JSONSoup):
+        d = soup.children
+      else:
+        return # All dicts should be wraped within JSONSoups
       if type(d) == dict:
-        for k,v in attrs.iteritems():
-          if k in d.keys() and d[k] == v:
-            yield d
+        if key_set.issubset(d.keys()):
+          yield soup
         for v in d.values():
           for _v in hunt(v):
             yield _v
@@ -77,9 +147,8 @@ class JSONSoup:
             yield _v
       return
     
-    results = [result for result in hunt(self.raw)]
-    result_soup = JSONSoup()
-    result_soup.raw = results
+    results = [result for result in hunt(self)]
+    result_soup = JSONSoup(results)
     return result_soup
       
 if __name__ == '__main__':
